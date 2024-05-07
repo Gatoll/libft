c94c4a10739f0118200bc0a7e68cd7bc64d65e50
Kanon Okazaki <kaokazak@c2r8s6.42tokyo.jp>
2024-05-07 20:32:24 +0900

    done

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: kaokazak <kaokazak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:43:27 by kaokazak          #+#    #+#             */
-/*   Updated: 2024/04/23 12:10:24 by kaokazak         ###   ########.fr       */
+/*   Updated: 2024/05/07 20:31:53 by kaokazak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char	*adr;
 	char	cc;
+	int		end;
 
-	adr = NULL;
 	cc = (char)c;
-	while (*s != '\0')
+	end = ft_strlen(s);
+	while (end >= 0)
 	{
-		if (*s == cc)
-			adr = (char *)s;
-		s++;
+		if (s[end] == cc)
+			return ((char *)&s[end]);
+		end--;
 	}
-	if (cc == '\0')
-		return ((char *)s);
-	return (adr);
+	return (NULL);
 }
